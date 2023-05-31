04ca3040ce57f2c73b1894f6a7073bbf2c8db1ac
Cullain <lukas.gedrat@gmx.net>
2023-05-31 11:05:06 +0200

    Packages

---
+++ com/cc/java/birds/Bird.java
@@ -0,0 +1,20 @@
+package com.cc.java.birds;
+
+import com.cc.java.interfaces.Flyable;
+import com.cc.java.interfaces.IFeathers;
+
+public class Bird implements Flyable, IFeathers {
+
+    @Override
+    public String fly() {
+        return "I'm a Bird, i can fly!";
+    }
+
+    @Override
+    public String hasFeathers() {
+        return "I have Feathers!";
+    }
+    
+
+    
+}
